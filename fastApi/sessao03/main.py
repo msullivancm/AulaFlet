76888963be863b578c4dea2aae477bd161528e05
= <=>
2024-04-17 15:52:40 -0300

    correção na estrutura do modelo de dados. Implementado endpoint post.

--- fastApi/sessao03/main.py
+++ fastApi/sessao03/main.py
@@ -1,6 +1,19 @@
+"""
+pip install fastapi 
+pip install uvicorn
+pip install fastapi2postman
+pip freeze > requirements.txt
+
+#para gerar arquivo de importação da api completa para o postman
+fastapi2postman --app nomedoarquivosemopontopy:app --output sessao03.json
+##caso o nome do arquivo seja main.py não precisa especificar o app
+fastapi2postman --app main --output sessao03.json
+"""
+
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
+from models import Curso
 
 app = FastAPI()
 
@@ -25,11 +38,22 @@ async def get_cursos():
 async def get_curso(curso_id: int):
     try: 
         curso = cursos[curso_id]
-        curso.update({"id": curso_id})
+        #curso.update({"id": curso_id})
         return curso
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado.')
-    
+
+@app.post('/cursos')
+async def post_curso(curso: Curso):
+    next_id: int = len(cursos) + 1
+    if curso.titulo not in cursos:
+        #exclui o atributo id do curso que, somente lá na frente será gerado por autonumeração do banco de dados. neste momento não é necessário.
+        del curso.id
+        cursos[next_id] = curso
+        return curso
+    else: 
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f'Já existe o curso com ID {curso.id}.')
 
 if __name__ == '__main__':
     import uvicorn 
