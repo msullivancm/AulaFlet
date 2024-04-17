3d55fbea442da9f014fbb030d04e4044bd36fff0
= <=>
2024-04-17 17:45:52 -0300

    parametro path para limitar retorno da api

--- fastApi/sessao03/main.py
+++ fastApi/sessao03/main.py
@@ -13,65 +13,83 @@ fastapi2postman --app main --output sessao03.json
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
+from fastapi import Path
 from models import Curso
 
 app = FastAPI()
 app.title = "Aula de FastAPI"
 
 cursos = {
-    1: {
-        "titulo": "Programação para leigos",
-        "aulas": 112,
-        "horas": 58
-    },
-    2: {
-        "titulo": "Algoritmos e Lógica de Programação",
-        "aulas": 87,
-        "horas": 67
-    }
+    1: {"titulo": "Programação para leigos", "aulas": 112, "horas": 58},
+    2: {"titulo": "Algoritmos e Lógica de Programação", "aulas": 87, "horas": 67},
+    3: {"titulo": "Algoritmos e Lógica de Programação 2", "aulas": 100, "horas": 70},
+    4: {"titulo": "Algoritmos e Lógica de Programação 3", "aulas": 140, "horas": 80},
+    5: {"titulo": "Algoritmos e Lógica de Programação 4", "aulas": 150, "horas": 90},
 }
 
-@app.get('/cursos')
+
+@app.get("/cursos")
 async def get_cursos():
     return cursos
 
-@app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
-    try: 
+
+@app.get("/cursos/{curso_id}")
+async def get_curso(
+    curso_id: int = Path(
+        title="ID do Curso",
+        description="Informe o ID do curso desejado entre 1 e 4.",
+        gt=0,
+        lt=5,
+    )
+):
+    try:
         curso = cursos[curso_id]
-        #curso.update({"id": curso_id})
+        # curso.update({"id": curso_id})
         return curso
     except KeyError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado.')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado."
+        )
 
-@app.post('/cursos', status_code=status.HTTP_201_CREATED )
+
+@app.post("/cursos", status_code=status.HTTP_201_CREATED)
 async def post_curso(curso: Curso):
     next_id: int = len(cursos) + 1
     if curso.titulo not in cursos:
-        #exclui o atributo id do curso que, somente lá na frente será gerado por autonumeração do banco de dados. neste momento não é necessário.
+        # exclui o atributo id do curso que, somente lá na frente será gerado por autonumeração do banco de dados. neste momento não é necessário.
         del curso.id
         cursos[next_id] = curso
         return curso
-    else: 
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail=f'Já existe o curso com ID {curso.id}.')
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Já existe o curso com ID {curso.id}.",
+        )
 
-@app.put('/cursos/{curso_id}')
+
+@app.put("/cursos/{curso_id}")
 async def put_curso(curso_id: int, curso: Curso):
     if curso_id in cursos:
         cursos[curso_id] = curso
         return curso
-    else: 
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Registro não encontrado.')
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Registro não encontrado."
+        )
 
-@app.delete('/cursos/{curso_id}', status_code=status.HTTP_204_NO_CONTENT)
+
+@app.delete("/cursos/{curso_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_curso(curso_id: int):
     if curso_id in cursos:
         del cursos[curso_id]
         return {"message": "Curso excluído com sucesso."}
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Registro não encontrado.')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Registro não encontrado."
+        )
 
-if __name__ == '__main__':
-    import uvicorn 
+
+if __name__ == "__main__":
+    import uvicorn
+
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
