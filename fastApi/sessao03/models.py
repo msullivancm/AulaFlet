3cea6f4d88401407bc5ef6434fa7d69ecc98186b
= <=>
2024-04-17 22:50:43 -0300

    Validações personalizadas no modelo de dados.

--- fastApi/sessao03/models.py
+++ fastApi/sessao03/models.py
@@ -1,12 +1,31 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class Curso(BaseModel):
     id: Optional[int] = None
     titulo: str
     aulas: int
     horas: int 
+
+    @validator('titulo')
+    def validate_titulo(cls, value):
+        if value.islower():
+            raise ValueError("O título deve ser capitalizado")
+        return value
     
+    @validator('aulas')
+    def validate_aulas(cls, value):
+        if value <= 0:
+            raise ValueError("O número de aulas deve ser maior que 0")
+        return value
+    
+    @validator('horas')
+    def validate_horas(cls, value):
+        if value <= 0:
+            raise ValueError("O número de horas deve ser maior que 0")
+        return value
+    
+        
 """ cursos = [
     Curso(id=1, titulo="Programação para leigos", aulas=112, horas=58),
     Curso(id=2, titulo="Algoritmos e Lógica de Programação", aulas=87, horas=67),
